1a74a68946937898f8ac2748973798eb02a54793
Hamza <hamzaredone6@gmail.com>
2024-10-21 22:21:46 +0000

    hope it woreks

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
 import { k } from "keyzilla";
 export default function Home() {
-  return <div>Hello World {k.NEXT_PUBLIC_QSD}</div>;
+  return <div>Hello World {k.NEXT_PUBLIC_KEYZILLA}</div>;
 }
---
+++ keyzilla.config.ts
@@ -0,0 +1,12 @@
+import { Config } from "keyzilla/config";
+
+export const config = Config({
+  production: {
+    projectName: "keyzilla",
+    envType: "org"
+  },
+  credentials: {
+    email: process.env.EMAIL,
+    secretCode: process.env.SECRET_CODE,
+  },
+});
